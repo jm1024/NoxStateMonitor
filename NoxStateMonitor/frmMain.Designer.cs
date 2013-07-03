cb66e23607204be0d0eba526ec2609edce81c1e5
James Martin <james@packrat.org>
2013-07-03 09:28:35 -0400

    NoxStateMonitor 0.04.00

---
+++ NoxStateMonitor/frmMain.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace NoxStateMonitor
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnToggle = new System.Windows.Forms.Button();
+            this.txtServerAddress = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.reader = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.point = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.state = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastSeen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.weight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRefresh = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnSnapshot = new System.Windows.Forms.Button();
+            this.cbMux = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnToggle
+            // 
+            this.btnToggle.Location = new System.Drawing.Point(449, 7);
+            this.btnToggle.Name = "btnToggle";
+            this.btnToggle.Size = new System.Drawing.Size(46, 23);
+            this.btnToggle.TabIndex = 0;
+            this.btnToggle.Text = "Start";
+            this.btnToggle.UseVisualStyleBackColor = true;
+            this.btnToggle.Click += new System.EventHandler(this.btnToggle_Click);
+            // 
+            // txtServerAddress
+            // 
+            this.txtServerAddress.Location = new System.Drawing.Point(98, 8);
+            this.txtServerAddress.Name = "txtServerAddress";
+            this.txtServerAddress.Size = new System.Drawing.Size(101, 20);
+            this.txtServerAddress.TabIndex = 1;
+            this.txtServerAddress.Text = "127.0.0.1:2090";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server Address";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.idName,
+            this.reader,
+            this.point,
+            this.state,
+            this.lastSeen,
+            this.weight});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(784, 519);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // id
+            // 
+            this.id.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.id.FillWeight = 88.83247F;
+            this.id.HeaderText = "Id";
+            this.id.MinimumWidth = 35;
+            this.id.Name = "id";
+            this.id.Width = 41;
+            // 
+            // idName
+            // 
+            this.idName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.idName.FillWeight = 101.8614F;
+            this.idName.HeaderText = "Name";
+            this.idName.Name = "idName";
+            this.idName.ReadOnly = true;
+            this.idName.Width = 60;
+            // 
+            // reader
+            // 
+            this.reader.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.reader.FillWeight = 101.8614F;
+            this.reader.HeaderText = "Reader";
+            this.reader.Name = "reader";
+            this.reader.ReadOnly = true;
+            // 
+            // point
+            // 
+            this.point.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.point.FillWeight = 101.8614F;
+            this.point.HeaderText = "Point";
+            this.point.Name = "point";
+            this.point.ReadOnly = true;
+            // 
+            // state
+            // 
+            this.state.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.state.FillWeight = 101.8614F;
+            this.state.HeaderText = "State";
+            this.state.Name = "state";
+            this.state.ReadOnly = true;
+            // 
+            // lastSeen
+            // 
+            this.lastSeen.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.lastSeen.FillWeight = 101.8614F;
+            this.lastSeen.HeaderText = "Last Seen";
+            this.lastSeen.Name = "lastSeen";
+            this.lastSeen.ReadOnly = true;
+            this.lastSeen.Width = 80;
+            // 
+            // weight
+            // 
+            this.weight.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.weight.DefaultCellStyle = dataGridViewCellStyle1;
+            this.weight.FillWeight = 101.8614F;
+            this.weight.HeaderText = "Weight";
+            this.weight.Name = "weight";
+            this.weight.ReadOnly = true;
+            this.weight.Width = 66;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(501, 7);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(46, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(205, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Refresh (ms)";
+            // 
+            // txtRefresh
+            // 
+            this.txtRefresh.Location = new System.Drawing.Point(271, 8);
+            this.txtRefresh.Name = "txtRefresh";
+            this.txtRefresh.Size = new System.Drawing.Size(42, 20);
+            this.txtRefresh.TabIndex = 6;
+            this.txtRefresh.Text = "1000";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(553, 12);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(79, 13);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "Not Connected";
+            // 
+            // btnSnapshot
+            // 
+            this.btnSnapshot.Location = new System.Drawing.Point(379, 7);
+            this.btnSnapshot.Name = "btnSnapshot";
+            this.btnSnapshot.Size = new System.Drawing.Size(64, 23);
+            this.btnSnapshot.TabIndex = 8;
+            this.btnSnapshot.Text = "Snapshot";
+            this.btnSnapshot.UseVisualStyleBackColor = true;
+            this.btnSnapshot.Click += new System.EventHandler(this.btnSnapshot_Click);
+            // 
+            // cbMux
+            // 
+            this.cbMux.AutoSize = true;
+            this.cbMux.Location = new System.Drawing.Point(326, 10);
+            this.cbMux.Name = "cbMux";
+            this.cbMux.Size = new System.Drawing.Size(46, 17);
+            this.cbMux.TabIndex = 9;
+            this.cbMux.Text = "Mux";
+            this.cbMux.UseVisualStyleBackColor = true;
+            this.cbMux.CheckStateChanged += new System.EventHandler(this.cbMux_CheckStateChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 557);
+            this.Controls.Add(this.cbMux);
+            this.Controls.Add(this.btnSnapshot);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtRefresh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtServerAddress);
+            this.Controls.Add(this.btnToggle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Nox State Monitor";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToggle;
+        private System.Windows.Forms.TextBox txtServerAddress;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn reader;
+        private System.Windows.Forms.DataGridViewTextBoxColumn point;
+        private System.Windows.Forms.DataGridViewTextBoxColumn state;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastSeen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn weight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRefresh;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnSnapshot;
+        private System.Windows.Forms.CheckBox cbMux;
+    }
+}
+
---
+++ NoxStateMonitor/frmMain.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace NoxStateMonitor
+{
+    public partial class frmMain : Form
+    {
+        public const String VERSION = "0.04.00";
+        private Boolean m_running = false;
+        private Boolean m_inTick = false;
+
+        /////////////////////////////////////////////////////
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        /////////////////////////////////////////////////////
+        private void startup()
+        {
+            this.Text = "Nox State Monitor " + VERSION;
+
+            dataGridView1.Width = this.ClientSize.Width;
+            dataGridView1.Height = this.ClientSize.Height - dataGridView1.Top;
+
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        /////////////////////////////////////////////////////
+        private void tick()
+        {
+            if (!m_running || m_inTick)
+            {
+                return;
+            }
+
+            m_inTick = true;
+
+            poll();
+
+            m_inTick = false;
+
+        }
+
+        /////////////////////////////////////////////////////
+        private void poll()
+        {
+            //set the cursor to busy and get the start time of the poll
+            Cursor.Current = Cursors.WaitCursor;
+
+            //get raw data from the server
+            String data = getDataFromServer(txtServerAddress.Text);
+
+            //parse and display the data
+            parseData(data);
+
+            //set the cursor to normal
+            Cursor.Current = Cursors.Default;
+        }
+
+        /////////////////////////////////////////////////////
+        private String getDataFromServer(String address)
+        {
+            String ret = "";
+
+            try
+            {
+                String URL = "";
+                if (cbMux.Checked)
+                {
+                    URL = "HTTP://" + address + "/?op=stateList";
+                }
+                else
+                {
+                    URL = "HTTP://" + address + "/?op=stateList&raw=true";
+                }
+                WebRequest request = WebRequest.Create(URL);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+
+                ret = reader.ReadToEnd();
+
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+            }
+            catch (Exception e)
+            {
+                lblStatus.Text = e.Message;
+                lblStatus.Refresh();
+                System.Threading.Thread.Sleep(500);
+            }
+
+            return ret;
+        }
+
+        /////////////////////////////////////////////////////
+        private void parseData(String rawData)
+        {
+            String[] lines = Regex.Split(rawData, System.Environment.NewLine);
+
+            double countPresent = 0;
+            double countAbsent = 0;
+            double countSquelch = 0;
+
+            String id = "";
+            String idName = "";
+            String reader = "";
+            String point = "";
+            String zone = "";
+            String state = "";
+            DateTime lastSeen = DateTime.Now;
+            Double weight = 0;
+
+            String tName = "";
+            String tValue = "";
+            foreach (String s in lines)
+            {
+                if (s == "")
+                {
+                    continue;
+                }
+                try
+                {
+                    String[] data = s.Split('|');
+                    tName = data[0];
+                    tValue = data[1];
+                    switch (tName)
+                    {
+                        case "id":
+                            if (id != "")
+                            {
+                                //add the item to the data grid
+                                if(cbMux.Checked)
+                                {
+                                    displayItemMuxed(id, idName, zone, state, lastSeen, weight);
+                                }
+                                else
+                                {
+                                    displayItemRaw(id, idName, reader, point, state, lastSeen, weight);
+                                }
+                                if (state == "present") { countPresent++; }
+                                if (state == "absent") { countAbsent++; }
+                                if (state == "squelch") { countSquelch++; }
+                            }
+                            id = tValue;
+                            break;
+                        case "idName":
+                            idName = tValue;
+                            break;
+                        case "reader":
+                            reader = tValue;
+                            break;
+                        case "point":
+                            point = tValue;
+                            break;
+                        case "zone":
+                            zone = tValue;
+                            break;
+                        case "state":
+                            state = tValue;
+                            break;
+                        case "lastSeen":
+                            lastSeen = DateTime.Parse(tValue);
+                            break;
+                        case "weight":
+                            weight = Convert.ToDouble(tValue);
+                            break;
+                    }
+                    
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Error parsing data: " + e.Message);
+                }
+            }
+
+            //add the last item if it exists
+            if (id != "")
+            {
+                //add the item to the data grid
+                if(cbMux.Checked)
+                {
+                    displayItemMuxed(id, idName, zone, state, lastSeen, weight);
+                }
+                else
+                {
+                    displayItemRaw(id, idName, reader, point, state, lastSeen, weight);
+                }
+                if (state == "present") { countPresent++; }
+                if (state == "absent") { countAbsent++; }
+                if (state == "squelch") { countSquelch++; }
+            }
+
+            //set the status text
+            lblStatus.Text = "p:" + countPresent.ToString() + "   a:" + countAbsent.ToString() + "   s:" + countSquelch.ToString();
+        }
+
+        /////////////////////////////////////////////////////
+        private void displayItemRaw(String id, String idName, String reader, String point, String state, DateTime lastSeen, Double weight)
+        {
+            Boolean bFound = false;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                try
+                {
+                    if (row.Cells[0].Value.ToString() == id && row.Cells[2].Value.ToString() == reader && row.Cells[3].Value.ToString() == point)
+                    {
+                        row.Cells[4].Value = state;
+                        row.Cells[5].Value = lastSeen;
+                        row.Cells[6].Value = weight;
+
+                        if (state == "present") { row.DefaultCellStyle.BackColor = Color.LawnGreen; }
+                        if (state == "absent") { row.DefaultCellStyle.BackColor = Color.LightCoral; }
+                        if (state == "squelch") { row.DefaultCellStyle.BackColor = Color.LemonChiffon; }
+
+                        bFound = true;
+                        break;
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            if (!bFound)
+            {
+                DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+                row.Cells[0].Value = id;
+                row.Cells[1].Value = idName;
+                row.Cells[2].Value = reader;
+                row.Cells[3].Value = point;
+                row.Cells[4].Value = state;
+                row.Cells[5].Value = lastSeen;
+                row.Cells[6].Value = weight;
+
+                if (state == "present") { row.DefaultCellStyle.BackColor = Color.LawnGreen; }
+                if (state == "absent") { row.DefaultCellStyle.BackColor = Color.LightCoral; }
+                if (state == "squelch") { row.DefaultCellStyle.BackColor = Color.LemonChiffon; }
+
+                dataGridView1.Rows.Add(row);
+            }
+
+
+        }
+        /////////////////////////////////////////////////////
+        private void displayItemMuxed(String id, String idName, String zone, String state, DateTime lastSeen, Double weight)
+        {
+            Boolean bFound = false;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                try
+                {
+                    if (row.Cells[0].Value.ToString() == id && row.Cells[2].Value.ToString() == zone)
+                    {
+                        row.Cells[4].Value = state;
+                        row.Cells[5].Value = lastSeen;
+                        row.Cells[6].Value = weight;
+
+                        if (state == "present") { row.DefaultCellStyle.BackColor = Color.LawnGreen; }
+                        if (state == "absent") { row.DefaultCellStyle.BackColor = Color.LightCoral; }
+                        if (state == "squelch") { row.DefaultCellStyle.BackColor = Color.LemonChiffon; }
+
+                        bFound = true;
+                        break;
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            if (!bFound)
+            {
+                DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+                row.Cells[0].Value = id;
+                row.Cells[1].Value = idName;
+                row.Cells[2].Value = zone;
+                row.Cells[3].Value = "n/a";
+                row.Cells[4].Value = state;
+                row.Cells[5].Value = lastSeen;
+                row.Cells[6].Value = weight;
+
+                if (state == "present") { row.DefaultCellStyle.BackColor = Color.LawnGreen; }
+                if (state == "absent") { row.DefaultCellStyle.BackColor = Color.LightCoral; }
+
+                dataGridView1.Rows.Add(row);
+            }
+
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////
+        #region events
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        /////////////////////////////////////////////////////
+        private void btnToggle_Click(object sender, EventArgs e)
+        {
+            if (m_running)
+            {
+                m_running = false;
+                btnToggle.Text = "Start";
+            }
+            else
+            {
+                try
+                {
+                    timer1.Interval = Convert.ToInt32(txtRefresh.Text);
+                }
+                catch (Exception)
+                {
+                    timer1.Interval = 1000;
+                    txtRefresh.Text = "1000";
+                }
+                m_running = true;
+                btnToggle.Text = "Stop";
+            }
+        }
+
+
+        /////////////////////////////////////////////////////
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+
+        /////////////////////////////////////////////////////
+        private void btnSnapshot_Click(object sender, EventArgs e)
+        {
+            poll();
+        }
+
+        /////////////////////////////////////////////////////
+        private void cbMux_CheckStateChanged(object sender, EventArgs e)
+        {
+            //need to clear the grid as results will be different
+            dataGridView1.Rows.Clear();
+
+            if (cbMux.Checked)
+            {
+                dataGridView1.Columns[2].HeaderText = "Zone";
+            }
+            else
+            {
+                dataGridView1.Columns[2].HeaderText = "Reader";
+            }
+
+        }
+
+        /////////////////////////////////////////////////////
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            startup();
+        }
+
+        /////////////////////////////////////////////////////
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            dataGridView1.Width = this.ClientSize.Width;
+            dataGridView1.Height = this.ClientSize.Height - dataGridView1.Top;
+        }
+
+        /////////////////////////////////////////////////////
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tick();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////
+        #endregion events
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////
+    }
+}
